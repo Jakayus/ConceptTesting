fd6e9fb406b1e57f52978262727e9ef111e623d0
Joel Sereno <jakayus123@gmail.com>
2023-08-02 22:17:34 -0700

    Add a custom Font
    - update info.plist and bundle information
    - add .ttf file to project
    - use SwiftUI modifier to use custom font

--- iOS/CustomFont/CustomFont/ContentView.swift
+++ iOS/CustomFont/CustomFont/ContentView.swift
@@ -5,6 +5,15 @@
 //  Created by Joel Sereno on 8/2/23.
 //
 
+/*
+ custom font printout
+ Arcade Classic
+ == ArcadeClassic
+ 
+ 
+ */
+
+
 import SwiftUI
 
 struct ContentView: View {
@@ -13,9 +22,17 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, world!").font(Font.custom("ArcadeClassic", size: 30))
         }
         .padding()
+        .onAppear{
+            for family in UIFont.familyNames {
+                     print(family)
+                     for names in UIFont.fontNames(forFamilyName: family){
+                     print("== \(names)")
+                     }
+                }
+        }
     }
 }
 
