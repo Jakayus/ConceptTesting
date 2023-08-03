76cc5e08efad06444fa8145ecf92c800f4a5c604
Joel Sereno <jakayus123@gmail.com>
2023-08-02 21:58:12 -0700

    Add Custom Font Project (initial state)

---
+++ iOS/CustomFont/CustomFont/CustomFontApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFontApp.swift
+//  CustomFont
+//
+//  Created by Joel Sereno on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomFontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
